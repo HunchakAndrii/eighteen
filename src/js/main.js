ffc5d313fb6d7d227dd7e737aa6737f10c0c83f5
HunchakAndrii <andreygunchak1@gmail.com>
2023-11-02 03:18:58 +0200

    start

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,65 @@
-const burger = document.querySelector('.burger')
 
-if(burger) {
+// const burger = document.querySelector('.burger')
+// const mobileMenu = document.querySelector('.mobile-menu')
+// const particlesContainer = document.querySelector('.particles-container')
+
+// if(burger) {
+//   burger.addEventListener('click', e => {
+//     e.preventDefault()
+
+//     for (let i = 0; i < 30; i++) {
+//       const particle = document.createElement('div')
+//       particle.classList.add('particle')
+//       particlesContainer.appendChild(particle)
+
+//       const x = Math.random() * 60
+//       const y = Math.random() * 60
+//       particle.style.left = `${x}px`
+//       particle.style.top = `${y}px`
+
+//       setTimeout(() => {
+//         particle.remove()
+//       }, 1000)
+//     }
+
+//     burger.classList.toggle('active')
+//     mobileMenu.classList.toggle('active')
+//   })
+// }
+
+const burger = document.querySelector('.burger')
+const mobileMenu = document.querySelector('.mobile-menu')
+const particlesContainer = document.querySelector('.particles-container')
+
+if (burger) {
   burger.addEventListener('click', e => {
     e.preventDefault()
 
+    for (let i = 0; i < 100; i++) {
+      const particle = document.createElement('div')
+      particle.classList.add('particle')
+      particlesContainer.appendChild(particle)
+
+      const x = Math.random() * 60
+      const y = Math.random() * 60
+      particle.style.left = `${x}px`
+      particle.style.top = `${y}px`
+
+      setTimeout(() => {
+        particle.remove()
+      }, 3000)
+    }
+
     burger.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
   })
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
 }
