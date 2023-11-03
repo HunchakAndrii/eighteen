ed6d4ea83100bed58c1e2d0e9800cbc0753bfa5b
HunchakAndrii <andreygunchak1@gmail.com>
2023-11-04 01:03:30 +0200

    make cool animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,65 @@
+import Lenis from '@studio-freight/lenis'
 
-// const burger = document.querySelector('.burger')
-// const mobileMenu = document.querySelector('.mobile-menu')
-// const particlesContainer = document.querySelector('.particles-container')
-
-// if(burger) {
-//   burger.addEventListener('click', e => {
-//     e.preventDefault()
-
-//     for (let i = 0; i < 30; i++) {
-//       const particle = document.createElement('div')
-//       particle.classList.add('particle')
-//       particlesContainer.appendChild(particle)
-
-//       const x = Math.random() * 60
-//       const y = Math.random() * 60
-//       particle.style.left = `${x}px`
-//       particle.style.top = `${y}px`
-
-//       setTimeout(() => {
-//         particle.remove()
-//       }, 1000)
-//     }
-
-//     burger.classList.toggle('active')
-//     mobileMenu.classList.toggle('active')
-//   })
-// }
 
 const burger = document.querySelector('.burger')
 const mobileMenu = document.querySelector('.mobile-menu')
-const particlesContainer = document.querySelector('.particles-container')
+const smoke = document.querySelector('.smoke')
+const body = document.body
 
-if (burger) {
+if(burger) {
   burger.addEventListener('click', e => {
     e.preventDefault()
 
-    for (let i = 0; i < 100; i++) {
-      const particle = document.createElement('div')
-      particle.classList.add('particle')
-      particlesContainer.appendChild(particle)
-
-      const x = Math.random() * 60
-      const y = Math.random() * 60
-      particle.style.left = `${x}px`
-      particle.style.top = `${y}px`
-
-      setTimeout(() => {
-        particle.remove()
-      }, 3000)
-    }
-
     burger.classList.toggle('active')
     mobileMenu.classList.toggle('active')
+    smoke.classList.toggle('active')
+    body.classList.toggle('active')
   })
 }
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF'
-  let color = '#'
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-  return color
+
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
 }
+
+requestAnimationFrame(raf) 
+
+
+
+var whiteSmokeItemStart = document.querySelector('#whitesmoke_2_')
+var whiteSmokeItemEnd = document.querySelector('#whitesmoke_56_')
+var whiteSmokeTlm = document.querySelector('#SMOKE1')
+
+whiteSmokeTlm = new gsap.timeline()
+
+
+whiteSmokeItemStart = {
+  y: 0,
+  x: 'random(-10, 30)',
+  scale: 0.5,
+  opacity: 0.3,
+}
+whiteSmokeItemEnd = {
+  y: -600,
+  x: 'random(-30, 40)',
+  stagger: {
+    each: 0.1,
+    repeat: -1,
+    repeatRefresh: true,
+  },
+  duration: 'random(5, 6)',
+  transformOrigin: '50% 50%',
+  scale: 'random(1, 2)',
+  opacity: 0,
+  ease: 'none',
+}
+
+
+whiteSmokeTlm
+  .fromTo('#whitesmokes circle', whiteSmokeItemStart, whiteSmokeItemEnd, 0)
+  .seek(100)
+
+
